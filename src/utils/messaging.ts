d00207826c8eb98b5c06e9d7bcc82f4c89841258
Prime-arys <88397986+Prime-arys@users.noreply.github.com>
2026-05-08 18:39:16 +0200

    refactor: reorganize ProtocolMap interface better clarity

--- src/utils/messaging.ts
+++ src/utils/messaging.ts
@@ -1,38 +1,44 @@
-import { defineExtensionMessaging } from '@webext-core/messaging';
-import { ConfigData } from '@/models/ConfigData';
-import { CommandsData } from '@/models/CommandsData';
+import { defineExtensionMessaging } from "@webext-core/messaging";
+import type { ConfigData } from "@/models/ConfigData";
+import type { CommandsData } from "@/models/CommandsData";
 
 export interface ProtocolMap {
-  // From Background to Content Script or popup (retrieved intents)
-  getConfig(): Promise<ConfigData>;
-  getCommands(): Promise<CommandsData>;
-  getCurrentDomain(): Promise<string>;  // will return the domain of the current active tab
+    /**
+     * send : Content Script / Popup
+     * on : Background
+     */
+    getConfig(): Promise<ConfigData>;
+    getCommands(): Promise<CommandsData>;
+    getCurrentDomain(): Promise<string>; // will return the domain of the current active tab
 
-  // From Content Script (received intents)
-  speedUp(): Promise<void>;
-  speedDown(): Promise<void>;
-  resetSpeed(): Promise<void>;
-  promptSpeed(): Promise<void>;
-  promptSpeedPropagation(playbackRate: number): Promise<void>;
-  getPlaybackRate(): Promise<number>;
-  pitchUp(): Promise<void>;
-  pitchDown(): Promise<void>;
-  resetPitch(): Promise<void>;
-  getPitch(): Promise<number>;
+    sendPlaybackRateUpdate(playbackRate: number): Promise<void>;
+    callSpeedUp(): Promise<void>;
+    callSpeedDown(): Promise<void>;
+    callResetSpeed(): Promise<void>;
+    callPromptSpeed(): Promise<void>;
+    callPitchUp(): Promise<void>;
+    callPitchDown(): Promise<void>;
+    callResetPitch(): Promise<void>;
 
-  // From Content Script or Popup (sended intents)
-  sendPlaybackRateUpdate(playbackRate: number): Promise<void>;
-  callSpeedUp(): Promise<void>;
-  callSpeedDown(): Promise<void>;
-  callResetSpeed(): Promise<void>;
-  callPromptSpeed(): Promise<void>;
-  callPitchUp(): Promise<void>;
-  callPitchDown(): Promise<void>;
-  callResetPitch(): Promise<void>;
+    // (popup)
+    retrieveCurrentPlaybackRate(): Promise<number | undefined>;
+    retrieveCurrentPitch(): Promise<number | undefined>;
 
-  // From Popup to Background to Content Script (retrieved intents)
-  retrieveCurrentPlaybackRate(): Promise<number | undefined>;
-  retrieveCurrentPitch(): Promise<number | undefined>;
+    /**
+     * send : Background
+     * on : Content Script
+     */
+    speedUp(): Promise<void>;
+    speedDown(): Promise<void>;
+    resetSpeed(): Promise<void>;
+    promptSpeed(): Promise<void>;
+    promptSpeedPropagation(playbackRate: number): Promise<void>;
+    getPlaybackRate(): Promise<number>;
+    pitchUp(): Promise<void>;
+    pitchDown(): Promise<void>;
+    resetPitch(): Promise<void>;
+    getPitch(): Promise<number>;
 }
 
-export const { sendMessage, onMessage } = defineExtensionMessaging<ProtocolMap>();
+export const { sendMessage, onMessage } =
+    defineExtensionMessaging<ProtocolMap>();
